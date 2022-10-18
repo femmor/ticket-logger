af8c53dc4f527b4bec78a8343a446bbc0d20cb9c
Emmanuel Egomson <fegomson@gmail.com>
2022-10-17 22:13:09 -0500

    Basic server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log('server...');
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const morgan = require('morgan');
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Ticket logger backend...');
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
