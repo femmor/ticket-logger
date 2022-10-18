9e16ebd79f21ccb8154e7ba053c993ab407427f2
Emmanuel Egomson <fegomson@gmail.com>
2022-10-18 01:10:12 -0500

    Creates Login and JWT User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
 /**
  *
@@ -43,6 +44,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: newUser._id,
       name: newUser.name,
       email: newUser.email,
+      token: generateToken(newUser._id),
     });
   } else {
     res.status(500);
@@ -58,8 +60,30 @@ const registerUser = asyncHandler(async (req, res) => {
  *
  */
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login user');
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email: email });
+
+  // Check if user and password match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(newUser._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid credentials');
+  }
 });
+
+// Generate token
+const generateToken = userId => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
 
 module.exports = {
   registerUser,
